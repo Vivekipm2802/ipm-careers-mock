cd76ba1f06ddea20ea84dccf6f24ab0686e9cca8
devankit1994 <devankit1994@gmail.com>
2025-09-04 19:25:42 +0530

    WIP

--- pages/test/components/QuestionCard.js
+++ pages/test/components/QuestionCard.js
@@ -1,20 +1,33 @@
-import { Button, Divider, Input, Radio, RadioGroup, ScrollShadow, Spacer } from "@nextui-org/react";
+import {
+  Button,
+  Divider,
+  Input,
+  Radio,
+  RadioGroup,
+  ScrollShadow,
+  Spacer,
+} from "@nextui-org/react";
 import { Check } from "lucide-react";
 
-export default function QuestionCard  ({question,onSelect,index,onReview,isMarked ,report}) {
-    if (!question) {
-      return <div>Question Undefined</div>;
-    }
-    const { id, title, type, questionimage, options, label } = question;
-    
-    const isDevelopment = process.env.NODE_ENV === 'development';
-  
+export default function QuestionCard({
+  question,
+  onSelect,
+  index,
+  onReview,
+  isMarked,
+  report,
+}) {
+  if (!question) {
+    return <div>Question Undefined</div>;
+  }
+  const { id, title, type, questionimage, options, label } = question;
 
-    
-    return (
-      <div className="font-sans w-full flex-1 flex flex-col text-left overflow-hidden">
-        <div className="rounded-xl w-full h-full flex flex-col items-start justify-start  relative overflow-y-auto">
-          <div className='w-full flex-1 h-full flex flex-col items-start justify-start p-4 lg:p-8'>
+  const isDevelopment = process.env.NODE_ENV === "development";
+
+  return (
+    <div className="font-sans w-full flex-1 flex flex-col text-left overflow-hidden">
+      <div className="rounded-xl w-full h-full flex flex-col items-start justify-start  relative overflow-y-auto">
+        <div className="w-full flex-1 h-full flex flex-col items-start justify-start p-4 lg:p-8">
           <h2 className="font-medium text-md text-primary">Question {index}</h2>
           <Divider className="my-2" />
           <h2 className="font-bold text-2xl text-primary">
@@ -22,32 +35,51 @@ export default function QuestionCard  ({question,onSelect,index,onReview,isMarke
           </h2>
           <Spacer y={4} />
           <div className="w-full flex flex-col flex-1 mb-auto">
-            <ScrollShadow 
-              className="font-medium [&_*]:sm:!text-sm [&_*]:!text-xs [&_*]:!font-normal [&_*]:!font-sans text-sm qcontent overflow-y-auto max-h-[40vh] lg:max-h-[40vh]" 
+            <ScrollShadow
+              className="font-medium [&_*]:sm:!text-sm [&_*]:!text-xs [&_*]:!font-normal [&_*]:!font-sans text-sm qcontent overflow-y-auto max-h-[40vh] lg:max-h-[40vh]"
               dangerouslySetInnerHTML={{ __html: question.question }}
             />
             <Spacer y={4} />
             <Divider />
             <Spacer y={4} />
-            {questionimage && <img className="max-h-[30vh]" src={questionimage} alt="Question" />}
+            {questionimage && (
+              <img
+                className="max-h-[30vh]"
+                src={questionimage}
+                alt="Question"
+              />
+            )}
             <ul className="p-0">
               {type === "options" && (
-                <RadioGroup 
-                isDisabled={report?.some(item=>item.id == question.id)}
-
-                  label={label || 'Select the correct option'}
-                  classNames={{label:"gradtext text-md font-bold"}}
-                  value={report?.find(item=>item.id == question.id)?.selectedOption}
-                  onValueChange={(e) => (report?.some(item=>item.id == question.id) ? '':onSelect({  selectedOption: e , ...question }))}
+                <RadioGroup
+                  label={label || "Select the correct option"}
+                  classNames={{ label: "gradtext text-md font-bold" }}
+                  value={
+                    report?.find((item) => item.id == question.id)
+                      ?.selectedOption
+                  }
+                  onValueChange={(e) =>
+                    onSelect({ selectedOption: e, ...question })
+                  }
                 >
                   {options.map((option, index) => (
-                    <Radio className="flex flex-row items-center justify-start" value={index + 1} key={index}>
+                    <Radio
+                      className="flex flex-row items-center justify-start"
+                      value={index + 1}
+                      key={index}
+                    >
                       {option.image ? (
-                        <img src={option.image} className="w-auto h-[64px] object-contain" alt={`Option ${index + 1}`} />
+                        <img
+                          src={option.image}
+                          className="w-auto h-[64px] object-contain"
+                          alt={`Option ${index + 1}`}
+                        />
                       ) : (
                         <>
                           <p className="text-sm">{option.title}</p>
-                          {isDevelopment && option.isCorrect && <div className="rounded-full w-1 h-1 bg-green-500" />}
+                          {isDevelopment && option.isCorrect && (
+                            <div className="rounded-full w-1 h-1 bg-green-500" />
+                          )}
                         </>
                       )}
                     </Radio>
@@ -68,13 +100,24 @@ export default function QuestionCard  ({question,onSelect,index,onReview,isMarke
                 </>
               )}
             </ul>
-          </div></div>
-          <div className='sticky bg-white border-t-1 w-full bottom-0 p-4'>
-  
-            <Button color={isMarked ? 'success':'primary'} variant={isMarked ? 'flat' : undefined} size='sm' onPress={()=>{isMarked  ? '' : onReview(question.id)}} startContent={isMarked && <Check className="text-success" size={16}></Check>}>{ isMarked ? 'Marked for Review': 'Mark this question for Review'}</Button>
           </div>
         </div>
+        <div className="sticky bg-white border-t-1 w-full bottom-0 p-4">
+          <Button
+            color={isMarked ? "success" : "primary"}
+            variant={isMarked ? "flat" : undefined}
+            size="sm"
+            onPress={() => {
+              isMarked ? "" : onReview(question.id);
+            }}
+            startContent={
+              isMarked && <Check className="text-success" size={16}></Check>
+            }
+          >
+            {isMarked ? "Marked for Review" : "Mark this question for Review"}
+          </Button>
+        </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+}
